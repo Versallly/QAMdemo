b8cc231a32ba25af8821408bcd8f89070011a9ee
Versallly <universallly@gmail.com>
2020-12-20 17:02:55 -0500

    comments deleted

--- cypress/integration/QAM/Paramount.spec.js
+++ cypress/integration/QAM/Paramount.spec.js
@@ -10,20 +10,17 @@ describe('Create mark - unmark as favorite', function(){
     it('Add project', function(){
         cy.contains('Add Project').click();
         cy.hash().should('include','#!/projects/new');
-        ;
         cy.get('label').contains('Project Name', {timeout:7000}).should('be.visible');
         cy.get('input[title="This field is required."]').type('Project QA 123');
         cy.get('input[data-bind="value: surveyCost"]').type('500');
         cy.get('input[data-bind="value: travelCost"]').type('1000');
         cy.contains('Save').should('be.enabled').click();
-        //cy.url().should('include', 'test');
     })
     it('Search the project', function(){
         cy.get('input[id="search"]').type('Project QA 123');
         cy.get('button').contains('Search').click();
         cy.location('hash').should('include','Project%20QA%20123');
         cy.get('h4').contains('Project QA 123').should('be.visible')
-        //cy.url().should('include', 'test');
     })
     it('Logout', function(){
         cy.contains('Logout').should('be.visible').click();
